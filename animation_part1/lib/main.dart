7970b7022e1d311729e26ead181563a35439ce8d
sultansingh7073 <sultankhilari7073@gmail.com>
2021-05-17 21:27:15 +0530

    work done

--- animation_part1/lib/main.dart
+++ animation_part1/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application.i have send you Offer Acceptancei have send you Offer Acceptancei have send you Offer Acceptance
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
