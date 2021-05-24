6f1092d110a444b511ab9011b36d0f7ca266a4ef
sultansingh7073 <sultankhilari7073@gmail.com>
2021-05-24 12:14:33 +0530

    work done

--- flutter_animation1/lib/main.dart
+++ flutter_animation1/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      //theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: HomePAge(),
     );
